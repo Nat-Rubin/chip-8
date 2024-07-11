f8112d23ba5a684104f383c9f5454d2f81342fcc
Nat Rubin <NathanielRubinM@gmail.com>
2024-07-10 22:48:20 -0400

    woo

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,21 @@
+use eframe::{egui, App, NativeOptions};
 
+pub struct Screen;
 
+impl Screen {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self
+    }
+}
+
+impl App for Screen {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello, world!");
+            if ui.button("Click me").clicked() {
+                ui.label("You clicked the button!");
+            }
+        });
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
 extern crate beryllium;
-extern crate gl33;
 
+use std::ffi::c_int;
 use beryllium::events::{Event, SDL_Scancode};
 use beryllium::*;
 use beryllium::init::*;
-use beryllium::video::{CreateWinArgs, GlWindow};
+use beryllium::video::{CreateWinArgs, GlWindow, RendererFlags, RendererWindow, RendererInfo};
 
-use gl33::*;
-use gl33::global_loader::{glBindBuffer, glClear, glClearColor, glGenBuffers, glGenVertexArrays};
+use rand::Rng;
 use chip8::Chip8;
+
 
 mod lib;
 mod stack;
 mod chip8;
 
-// return error when an error accurs lmao
-fn execute_instruction(chip8: &mut Chip8, window: &mut GlWindow) {
+fn sdl_draw(bitmap: &[[u16; 32]; 64], renderer: RendererWindow) {
+    renderer.set_draw_color(0, 0, 0, 255).unwrap();
+    renderer.clear().unwrap();
+    renderer.set_draw_color(255, 255, 255, 255).unwrap();
+    let scale: c_int = 20;
+    for i in 0..bitmap.len() {
+        for j in 0..bitmap[i].len() {
+            let x: c_int = i as c_int;
+            let y: c_int = j as c_int;
+            let p1: [c_int; 2] = [x, x*scale];
+            let p2: [c_int; 2] = [y, y*scale];
+            let points: [[c_int; 2]; 2] = [p1, p2];
+            renderer.draw_lines(&points).unwrap();
+        }
+    }
+}
+
+// return error when an error occurs lmao
+fn execute_instruction(chip8: &mut Chip8, window: &mut RendererWindow) {
     // fetch
-    chip8.mem[chip8.pc as usize] = 0b00111111;
-    chip8.mem[chip8.pc as usize +1] = 0b00111100;
+    chip8.mem[chip8.pc as usize] = 0b11011010;
+    chip8.mem[chip8.pc as usize +1] = 0b10101100;
     let instruct: u16 = ((chip8.mem[chip8.pc as usize] as u16) << 8) + chip8.mem[(chip8.pc as usize)+1] as u16;
     chip8.pc += 2;
     // decode & execute
@@ -36,7 +53,7 @@ fn execute_instruction(chip8: &mut Chip8, window: &mut GlWindow) {
                                 0x0 => {
                                     // clear screen
                                     unsafe {
-                                        glClear(GL_COLOR_BUFFER_BIT);
+                                        //glClear(GL_COLOR_BUFFER_BIT);
                                     }
                                 }
                                 0xE => {
@@ -153,7 +170,9 @@ fn execute_instruction(chip8: &mut Chip8, window: &mut GlWindow) {
                     unsafe {
                         let vx_ptr = v0_ptr.offset((nib_1 as u8) as isize);
                         let vy_ptr = v0_ptr.offset((nib_2 as u8) as isize);
-                        match *vx_ptr.checked_add(*vy_ptr) {
+                        let vx_as_num = *vx_ptr;
+                        let vy_as_num = *vy_ptr;
+                        match vx_as_num.checked_add(vy_as_num) {
                             Some(_) => {
                                 *vx_ptr += *vy_ptr;
                                 chip8.VF = 0;
@@ -171,7 +190,9 @@ fn execute_instruction(chip8: &mut Chip8, window: &mut GlWindow) {
                     unsafe {
                         let vx_ptr = v0_ptr.offset((nib_1 as u8) as isize);
                         let vy_ptr = v0_ptr.offset((nib_2 as u8) as isize);
-                        match *vx_ptr.checked_sub(*vy_ptr) {
+                        let vx_as_num = *vx_ptr;
+                        let vy_as_num = *vy_ptr;
+                        match vx_as_num.checked_sub(vy_as_num) {
                             Some(_) => {
                                 *vx_ptr -= *vy_ptr;
                                 chip8.VF = 1;
@@ -201,7 +222,9 @@ fn execute_instruction(chip8: &mut Chip8, window: &mut GlWindow) {
                     unsafe {
                         let vx_ptr = v0_ptr.offset((nib_1 as u8) as isize);
                         let vy_ptr = v0_ptr.offset((nib_2 as u8) as isize);
-                        match *vy_ptr.checked_sub(*vx_ptr) {
+                        let vx_as_num = *vx_ptr;
+                        let vy_as_num = *vy_ptr;
+                        match vy_as_num.checked_sub(vx_as_num) {
                             Some(_) => {
                                 *vy_ptr -= *vx_ptr;
                                 chip8.VF = 1;
@@ -251,10 +274,46 @@ fn execute_instruction(chip8: &mut Chip8, window: &mut GlWindow) {
         0xC => {
             // set VX to rand num & NN
             let v0_ptr: *mut u8 = &mut chip8.V0;
+            let NN:u8 = ((nib_2 << 4) + nib_3) as u8;
+            let mut rng = rand::thread_rng();
+            let rand_num: u8 = (rng.gen::<u8>());
             unsafe {
                 let vx_ptr = v0_ptr.offset((nib_1 as u8) as isize);
+                *vx_ptr = (rand_num & NN);
             }
         },
+        0xD => {
+            // TODO: this
+            // display
+            // draws N pixels tall sprite from mem location that I reg is holding to the screen
+            // VX, VY coords
+            // all pixels that are "on" will flip pixels on screen
+            // drawn left to right, most to least significant bit
+            // if any pixels turned "off", set VF flag to 1, else set to 0
+            let x: u8;
+            let y: u8;
+            let v0_ptr = std::ptr::addr_of!(chip8.V0);
+            unsafe {
+                let vx_ptr = v0_ptr.offset((nib_1 as u8) as isize);
+                let vy_ptr = v0_ptr.offset((nib_2 as u8) as isize);
+                x = *vx_ptr & (chip8.width-1) as u8;
+                y = *vy_ptr & (chip8.height-1) as u8;;
+            }
+        },
+        0xE => {
+            match nib_2 {
+                0x9 => {
+                    match nib_3 {
+                        0xE => {
+                            // Checks if key is held down, PC += 2
+
+                        }
+                        _ => println!("command not implemented"),
+                    }
+                }
+                _ => println!("command not implemented"),
+            }
+        }
         _ => println!("command not implemented"),
     }
 }
@@ -296,9 +355,19 @@ fn main() {
         borderless: false,
         resizable: false,
     };
-    let mut window = sdl.create_gl_window(
-        win_args,
-    ).expect("Failed to make window :(");
+    //let mut window = sdl.create_gl_window(
+    //    win_args,
+    //).expect("Failed to make window :(");
+    let win_args = CreateWinArgs{
+        title: "Chip-8",
+        width: chip8.width*chip8.scale,
+        height: chip8.height*chip8.scale,
+        allow_high_dpi: false,
+        borderless: false,
+        resizable: false,
+    };
+    let render_flags: RendererFlags = <RendererFlags as std::default::Default>::default();
+    let mut renderer = sdl.create_renderer_window(win_args, render_flags,).unwrap();
 
     // unsafe {
     //     let mut vao = 0;
@@ -323,7 +392,7 @@ fn main() {
                         21 => println!("r"),
                         _ => println!("other scancode"),
                     }
-                    execute_instruction(&mut chip8, &mut window)  // TODO: fix timing, run at 60fps
+                    execute_instruction(&mut chip8, &mut renderer)  // TODO: fix timing, run at 60fps
                 }
                 _ => (),
             }
